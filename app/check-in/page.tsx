c29fb43341b6150d0f9f836ace8c0e317a941ec8
hxhhxxs <100739866+hxhhxxs@users.noreply.github.com>
2026-05-15 20:45:32 -0500

    Remove separate mission panel above log

--- app/check-in/page.tsx
+++ app/check-in/page.tsx
@@ -1,6 +1,5 @@
 import CheckInMain from "./check-in-main";
-import { MissionPanel } from "./mission-panel";
 
 export default function CheckInPage() {
-  return <><MissionPanel /><CheckInMain /></>;
+  return <CheckInMain />;
 }
