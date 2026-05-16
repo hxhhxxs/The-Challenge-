f0911ab845ff592c4b0177215e2335634ba0a5a5
hxhhxxs <100739866+hxhhxxs@users.noreply.github.com>
2026-05-15 20:33:39 -0500

    Show rotating missions on check-in page

--- app/check-in/page.tsx
+++ app/check-in/page.tsx
@@ -1,5 +1,6 @@
 import CheckInMain from "./check-in-main";
+import { MissionPanel } from "./mission-panel";
 
 export default function CheckInPage() {
-  return <CheckInMain />;
+  return <><MissionPanel /><CheckInMain /></>;
 }
